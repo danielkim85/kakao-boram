2dbb32fe297633538563b8ce428f30c75bb71957
DK <danielkim@soulkast.com>
2020-01-10 03:06:57 -0500

    added socket implementation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,94 @@
 angular.module("app", []).controller("BoramCtrl", function($scope) {
-  $scope.logout = function(){
-    Kakao.Auth.logout(function(){
-      console.info('logged out');
+
+  const socket = io();
+  const DEFAULT_PROFILE_IMG = 'https://www.downeastyachting.com/wp/wp-content/uploads/downeastyachting.com/2005/09/default-profile.png';
+  Kakao.init('72ec3baa9e089759a3d2618025dfc1f8');
+
+  $scope.profile = {
+    userId : undefined,
+    nickname : undefined,
+    thumbnailImage : undefined,
+    accessToken : undefined
+  };
+
+  Kakao.Auth.createLoginButton({
+    container: '#kakao-login-btn',
+    scope: 'profile,friends,talk_message',
+    success: function(authObj) {
+      profile.accessToken = authObj.access_token;
+      getStatusInfo();
+    },
+    fail: function(err) {
+      console.error(err);
+    }
+  });
+
+  const getStatusInfo = function (){
+    Kakao.Auth.getStatusInfo(function(statusObj){
+      $scope.isLoggedIn = statusObj.status === 'connected';
+      if($scope.isLoggedIn){
+        loadTestData();
+        loadProfile(statusObj.user);
+        if(!$scope.profile.accessToken){
+          $scope.profile.accessToken = Kakao.Auth.getAccessToken();
+        }
+        socket.emit('join',{
+          accessToken : $scope.profile.accessToken,
+          userId : $scope.profile.userId
+        });
+        $scope.$apply();
+      }
     });
   };
+
+  const loadProfile = function(user){
+    $scope.profile.userId = user.id;
+    $scope.profile.nickname = user.properties.nickname;
+    $scope.profile.thumbnailImage = user.properties.thumbnail_image;
+  };
+
+  const loadTestData = function(){
+    $scope.chats = [
+      {
+        profile : {
+          nickname : 'Danny',
+          thumbnailImage : 'http://k.kakaocdn.net/dn/beFkCV/btquxpiy2wH/lO3QFHRqmIk08mIHsiWxa0/img_110x110.jpg'
+        },
+        msg : 'test msg'
+      }
+    ];
+  };
+
+  getStatusInfo();
+
+  $scope.logout = function(){
+    Kakao.Auth.logout(function(){
+      $scope.isLoggedIn = false;
+      $scope.$apply();
+    });
+  };
+
+  $scope.send = function(msg) {
+    socket.emit('msg',{
+      profile : $scope.profile,
+      msg : msg
+    });
+    $scope.msg = '';
+  };
+
+  socket.on('refresh',function(){
+    window.location.reload();
+  });
+
+  socket.on('msg',function(data){
+    if(!data.profile.thumbnailImage){
+      data.profile.thumbnailImage = DEFAULT_PROFILE_IMG;
+    }
+    $scope.chats.push(data);
+    $scope.$apply();
+  });
+
+  /*
   $scope.friends = function(){
     Kakao.API.request({
       url: '/v1/api/talk/friends',
@@ -15,4 +100,47 @@ angular.module("app", []).controller("BoramCtrl", function($scope) {
       }
     });
   };
+
+  $scope.message = function(){
+    Kakao.API.request({
+      url: '/v2/api/talk/memo/default/send',
+      data: {
+        template_object: {
+          "object_type":"text",
+          "text":"텍스트 영역입니다. 최대 200자 표시 가능합니다.",
+          "link":{
+            "web_url":"http://dev.kakao.com",
+            "mobile_web_url":"http://dev.kakao.com"
+          },
+          "button_title":""
+        }
+      },
+      success: function(res) {
+        alert('메시지 전송이 완료되었습니다.');
+      }
+    });
+
+    Kakao.API.request({
+      url: '/v1/api/talk/friends',
+      success: function(res) {
+        // 친구 중에 첫 번째 친구에게 메시지를 보냅니다. API 호출 시 메시지가 전송됨으로 테스트 시 주의해주세요.
+        console.info('message sending to');
+        console.info(res.elements[0]);
+        Kakao.API.request({
+          url: '/v1/api/talk/friends/message/default/send',
+          data: {
+            receiver_uuids: [res.elements[0].uuid],
+            template_object: {
+              "object_type":"text",
+              "text":"test message.",
+            }
+          },
+          success: function(res) {
+            alert('메시지 전송이 완료되었습니다.');
+          }
+        });
+      }
+    });
+  };
+  */
 });
