adb23bf8ff6b0dedd1c43a0d79ced27bfc083f39
DK <danielkim@soulkast.com>
2020-02-02 02:00:49 -0500

    adding some debug logic in prod

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -137,7 +137,6 @@ angular.module("app", ['ngSanitize','ngCookies']).controller("BoramCtrl", functi
   });
 
   $scope.debug = function(){
-    alert(isMobile());
     console.info('debug called');
     socket.emit('debug');
   };
